10138b3db2c8ce230dafa9b816ec86dbf833811a
Jormund123 <anand.karna.social@gmail.com>
2022-06-23 15:15:03 +0545

    Rightbar Completed

--- client/src/components/Rightbar/Rightbar.js
+++ client/src/components/Rightbar/Rightbar.js
@@ -2,5 +2,27 @@ import React from "react";
 import "./rightbar.css";
 
 export default function Rightbar() {
-    return <div className='rightbar'>Rightbar</div>;
+    return (
+        <div className='rightbar'>
+            <div className='rightbarWrapper'>
+                <div className='birthdayContainer'>
+                    <img src='assets/gift.png' className='birthdayImg' alt='' />
+                    <span className='birthdayText'>
+                        <b> Arjun </b> and <b> 3 other Friends </b> have birthdays today.
+                    </span>
+                </div>
+                <img className='rightbarAd' src='assets/ad.jpg' alt='' />
+                <h4 className='rightbarTitle'>Online Friends</h4>
+                <ul className='rightbarFriendList'>
+                    <li className='rightbarFriend'>
+                        <div className='rightbarProfileImgContainer'>
+                            <img src='assets/person/3.jpeg' className='rightbarProfileImg' alt='' />
+                            <span className='rightbarOnline'></span>
+                        </div>
+                        <span className='rightbarUsername'>John Doe</span>
+                    </li>
+                </ul>
+            </div>
+        </div>
+    );
 }
