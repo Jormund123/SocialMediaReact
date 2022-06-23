1a422b959b189aeab20795b3df8f4f36081b4bb0
Jormund123 <anand.karna.social@gmail.com>
2022-06-23 14:57:29 +0545

    Feed completed

--- client/src/components/Feed/Feed.js
+++ client/src/components/Feed/Feed.js
@@ -10,6 +10,8 @@ export default function Feed() {
                 <div className='feedWrapper'>
                     <Share />
                     <Post />
+                    <Post />
+                    <Post />
                 </div>
             </div>
         </>
